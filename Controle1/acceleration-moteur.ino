d0eb0a2cfcde533a0f7c94ed9dd9d85198821f7c
MushuLeDragon <22367990+MushuLeDragon@users.noreply.github.com>
2018-04-17 10:44:24 +0200

    Create acceleration-moteur.ino

---
+++ Controle1/acceleration-moteur.ino
@@ -0,0 +1,22 @@
+#define PIN_MOTOR 9
+#define PIN_POT A0
+#define INCREMENT_TIME (1000.0/(0.1*255))
+
+void setup() {
+    pinMode(PIN_POT, INPUT);
+    pinMode(PIN_MOTOR, OUTPUT);
+    analogWrite(PIN_MOTOR, 0);
+}
+
+void loop() {
+    int consigne = map(analoRead(PIN_POT), 0, 1023, 0, 255);
+
+    static int previous = 0;
+    static unsigned long refTime = millis();
+
+    if(millis()>= (refTime + INCREMENT_TIME)) {
+        refTime = millis();
+        if (consigne > previous) analogWrite(PIN_MOTOR, ++previous);
+        elseif(consigne < previous) analogWrite(PIN_MOTOR, --previous)
+    }
+}
